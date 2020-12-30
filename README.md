c16e62c80a1deb8d603b18e408667fc8e932f371
igor vinicius Freitas de Souza <43221464+igor1043@users.noreply.github.com>
2020-12-30 15:28:03 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ListaInterfaceGrafica
-Este e um pequeno programa em C ++ que cria uma lista de 10 strings usando um laço de repetição para.
+# listas_GUI
+
+Este e um pequeno programa em C++ que cria lista de 10 strings usando o laco de repeticao for.
+
+![Listas](https://github.com/lucasbat/listas_GUI/blob/master/Screenshot.png?raw=true)
+
+
+
---
+++ listas.cpp
@@ -0,0 +1,55 @@
+#include "listas.h"
+#include "ui_listas.h"
+
+//add biblioteca QMessageBox
+#include "QMessageBox"
+
+//add biblioteca QString
+#include "QString"
+
+
+Listas::Listas(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Listas)
+{
+    ui->setupUi(this);
+}
+
+Listas::~Listas()
+{
+    delete ui;
+}
+
+QString matriz[10];
+int i=0;
+
+//adicionar itens a uma lista
+void Listas::on_btn_add_clicked()
+{
+
+    matriz[i] = ui->lineEdit->text();
+    i++;
+
+    ui->lineEdit->clear();
+}
+
+//listar os itens
+void Listas::on_btn_list_clicked()
+{
+    ui->lista->clear();
+    for(int i=0; i<10; i++)
+    ui->lista->addItem(matriz[i]);
+}
+
+
+//botao delete
+void Listas::on_btn_del_clicked()
+{
+    ui->lista->takeItem(ui->lista->row(ui->lista->currentItem()));
+}
+
+void Listas::on_btn_select_clicked()
+{
+    QString sele=ui->lista->currentItem()->text();
+    QMessageBox::information(NULL,"Item","Item Selecionado: \n" +sele);
+}
